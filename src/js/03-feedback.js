6319cccc8e403e7038844e28fad7322332473f3d
some3thing4 <107303649+some3thing4@users.noreply.github.com>
2022-10-26 00:07:38 +0300

    add function for read data from storage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,55 +2,59 @@ import {save, load, remove } from "./partials/storage";
 import throttle from "lodash.throttle";
 
 const form = document.querySelector('.feedback-form');
-const {email, message} = form;
-const LOCALSTORAGE_KEY = "feedback-form-state";
-const formData = {};
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+const LOCAL_STORAGE_KEY = "feedback-form-state";
+let formData = {};
+
+
+onStorageData();
+readDataFromStorage();
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onFormInput, 500));
 
-onStorageData();
-
 function onFormInput(event) {
-    const {name, value} = event.target;
-    formData[name] = value;
-    
-    save(LOCALSTORAGE_KEY, formData);
+  const { name, value } = event.target;
+
+  formData[name] = value;
+  save(LOCAL_STORAGE_KEY, formData);
 }
 
 function onFormSubmit(event) {
-    event.preventDefault();
-
-    // const {
-    //     elements: { email, message },
-    //   } = event.currentTarget;
-    if (email.value && message.value) {
-        console.log({ email: email.value, message: message.value });
-        event.currentTarget.reset();
-        remove(LOCALSTORAGE_KEY);   ;
-    } else alert('Please, fill all fields!');
+  event.preventDefault();
+  
+  if (email.value && message.value) {
+      console.log(formData);
+      event.currentTarget.reset();
+      remove(LOCAL_STORAGE_KEY);   ;
+  } else alert('Please, fill all fields!');
 }
 
-function onStorageData(event) {
-    const savedData = load(LOCALSTORAGE_KEY);
-    try {
-      email.value = savedData.email;
-      message.value = savedData.message;
-    } catch (error) {
-      console.log('Storage is empty');
-    }
+function readDataFromStorage() {
+  formData = load(LOCAL_STORAGE_KEY);
+  formData = formData ? formData : {};
+}
+
+function onStorageData() {
+  formData = load(LOCAL_STORAGE_KEY);
+
+  if (!formData) {
+    return;
   }
+  Object.entries(formData).forEach(([key, value]) => {
+    form.elements[key].value = value;
+  });
+}
 
-//   Alternative for load stored data 
-// function onStorageData() {
-//     const savedData = load(LOCALSTORAGE_KEY);
 
-//     if (savedData) {
-//        Object.entries(savedData).forEach(([key, value]) => {
-//             form.elements[key].value = value;
-//         });
-//     }
-// }
+
+
+
+
+
+
+
 
 
 
